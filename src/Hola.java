d38e34f7a47b496d79143119fbb9021d3965c73a
ko2ja <ko2ja@DESKTOP-4P6SQK2>
2023-04-26 12:11:35 +0200

    Javi yeahhh!

--- src/Hola.java
+++ src/Hola.java
@@ -4,7 +4,8 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Ceinmark");
 		System.out.println("Hola, soy Marta");
-
+		System.out.println(" Ko2 is on the house!!!!");
+		System.out.println("Javi de nuevo");
 	}
 
 }
